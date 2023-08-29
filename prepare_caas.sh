92c3f439d1568b6b2f96287d1d1b386d1f2587fa
Bugstone <greenduck83@gmail.com>
2023-08-29 16:15:52 +0900

    Update prepare_caas.sh

--- prepare_caas.sh
+++ prepare_caas.sh
@@ -1,5 +1,5 @@
 echo "Downloading jacov-maven-plugin.jar"
-wget -O jacov-maven-plugin.jar https://suresofttech.hanbiro.net/cgi-bin/cloudGetLink.cgi?id=66971&key=87573334efa9eff6757ab39271f673fb
+wget -O jacov-maven-plugin.jar https://suresofttech.hanbiro.net/cgi-bin/cloudGetLink.cgi?id=66973&key=717959419dd90a814136bf2acdc39cd9
 echo "Complete download. (jacov-maven-plugin)"
 echo "Downloading java-runtime.jar"
 wget -O java-runtime.jar https://suresofttech.hanbiro.net/cgi-bin/cloudGetLink.cgi?id=66972&key=ddf8687c7c4a984bbc6db42bf6053ded
