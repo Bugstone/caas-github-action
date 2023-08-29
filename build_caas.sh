7af9a702f34e21fa47ef40f3848841113fd8f97d
Bugstone <greenduck83@gmail.com>
2023-08-29 13:56:59 +0900

    Update build_caas.sh

--- build_caas.sh
+++ build_caas.sh
@@ -1,1 +1,4 @@
+echo "Coverage Maven Build..."
+echo "mvn com.qualityscroll.caas:jacov-maven-plugin:1.0.0-SNAPSHOT:setup $* -Dmaven.jacov.addDependencyJavaLib=true"
 mvn com.qualityscroll.caas:jacov-maven-plugin:1.0.0-SNAPSHOT:setup $* -Dmaven.jacov.addDependencyJavaLib=true
+echo "Build Finished"
