0a8544f613382c253103b902dd2913165d9824ec
Bugstone <greenduck83@gmail.com>
2023-08-29 16:44:05 +0900

    Update prepare_caas.sh

--- prepare_caas.sh
+++ prepare_caas.sh
@@ -1,9 +1,8 @@
-rm -rf /home/runner/.m2/repository/com/qualityscroll/caas/jacov-maven-plugin
 echo "Downloading jacov-maven-plugin.jar"
-wget -O jacov-maven-plugin.jar "https://drive.google.com/file/d/10ungQObnmFZYRHiolSPUD1L9FUU1GdeY/view?usp=sharing"
+curl -O "https://drive.google.com/file/d/10ungQObnmFZYRHiolSPUD1L9FUU1GdeY/view?usp=sharing"
 echo "Complete download. (jacov-maven-plugin)"
 echo "Downloading java-runtime.jar"
-wget -O java-runtime.jar "https://suresofttech.hanbiro.net/cgi-bin/cloudGetLink.cgi?id=66972&key=ddf8687c7c4a984bbc6db42bf6053ded"
+curl -O "https://suresofttech.hanbiro.net/cgi-bin/cloudGetLink.cgi?id=66972&key=ddf8687c7c4a984bbc6db42bf6053ded"
 echo "Complete download. (java-runtime)"
 
 echo "Install maven plugins (jacov-maven-plugin)"
